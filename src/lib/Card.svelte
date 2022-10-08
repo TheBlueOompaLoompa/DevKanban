103538d17e8acc7fb3d9a5adbfa0d9a673480dfa
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2022-10-08 01:03:08 -0500

    Fix card

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
     export let id: string;
-    export let title: string;
+    export let name: string;
 </script>
 
 <card>
-    <p>{title}</p>
+    <p>{name}</p>
 </card>
+
+<style>
+    card {
+        background-color: rgba(0, 0, 0, 0.2);
+        width: 90%;
+
+        border-radius: var(--curvy);
+
+        margin: 3px 0px 3px 0px;
+    }
+
+    p {
+        margin: 10px;
+    }
+</style>
--- src/lib/List.svelte
+++ src/lib/List.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { supabase } from './supabase.js';
+  import { supabase, createThing } from './supabase.js';
   import Card from "./Card.svelte";
 
   export let id: string;
@@ -7,30 +7,40 @@
 
   let cards = [];
 
-  async function loadCards() {
-    const res = await supabase.from('cards').select('*').eq('list', id);
+  async function createCard() {
+    await createThing('cards', 'list', id, 'card');
+  }
 
-    if(res.status.toString().startsWith('20')) {
-      console.log(res.body)
-      cards = res.body;
+  async function loadCards() {
+    let { data, error } = await supabase.from('cards').select('*').eq('list', id);
+
+    if(!error) {
+      cards = data;
     }
   }
 
   loadCards().then(() => {
     supabase.from('lists').on('*', loadCards);
-  })
+  });
 </script>
 
 <section>
   <p>{name}</p>
   <cards>
   {#each cards as card}
-    <Card id={card.id} title={card.title}/>
+    <Card id={card.id} name={card.name}/>
   {/each}
+    <button on:click={createCard}>Create Card</button>
   </cards>
 </section>
 
 <style>
+  section {
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: var(--curvy);
+    margin-right: 10px;
+  }
+
   section, cards {
     height: 100%;
     display: flex;
